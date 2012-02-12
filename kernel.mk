d93a0dad13e40c9f33563d528cd3075c5a3bf408
podxboq <podxboq@gmail.com>
2012-02-12 11:12:45 +0100

    Variables on this file not make effect on AOSP standard definition.

--- kernel.mk
+++ kernel.mk
@@ -1,7 +1,3 @@
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 msmsdcc_sdioirq=1 wire.search_count=5
-BOARD_KERNEL_BASE := 0x20000000
-BOARD_KERNEL_NEW_PPPOX := true
-
 ST_KERNEL_DIR := kernel/bravo
 ST_KERNEL_CFG := device/htc/bravo/superteam_bravo_defconfig
 ST_KERNEL_IMG := device/htc/bravo/kernel
