71d610397dce41c50cef8301958882c093fc41fc
podxboq <podxboq@gmail.com>
2012-11-15 19:24:41 +0100

    Filter kernel.mk file only for bravo device

--- kernel.mk
+++ kernel.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_DEVICE),bravo)
 ST_KERNEL_DIR := kernel/bravo
 ST_KERNEL_CFG := device/htc/bravo/superteam_bravo_defconfig
 ST_KERNEL_IMG := device/htc/bravo/kernel
@@ -10,3 +11,4 @@ BOARD_KERNEL_NEW_PPPOX := true
 
 PRODUCT_COPY_FILES += \
     device/htc/bravo/kernel:kernel
+endif
